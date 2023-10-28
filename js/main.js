6dcb2f1ff4f1e5ad53a6ecada2d69aa18dd2f49b
kenbugless <kenbugless@gmail.com>
2023-10-28 10:07:16 +0900

    닫기 모션 이벤트 핸들러 연결

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ const main = document.querySelector('main');
 const [topEl, rightEl, bottomEl, leftEl, conEl] = main.children;
 const btn = document.querySelector('button');
 const speed = 500;
+const btnclose = conEl.querySelector('span');
 
 btn.addEventListener('click', panelOpen);
+btnclose.addEventListener('click', panelClose);
 
 function panelOpen() {
 	new Anime(
@@ -46,3 +48,22 @@ function panelOpen() {
 		}
 	);
 }
+function panelClose() {
+	conEl.classList.remove('on');
+
+	setTimeout(() => {
+		[(leftEl, rightEl)]
+			.forEach((el) => new Anime(el, { height: '0%' }, { duration: speed }))
+
+			[(topEl, bottomEl)].forEach((el) => new Anime(el, { width: '0%' }, { duration: speed }));
+	}, 1000);
+
+	// new Anime(leftEl, { height: '0%' }, { duration: speed });
+	// new Anime(rightEl, { height: '0%' }, { duration: speed });
+	// new Anime(topEl, { width: '0%' }, { duration: speed });
+	// new Anime(bottomEl, { width: '0%' }, { duration: speed });
+
+	btn.classList.remove('off');
+}
+
+//미션 - 닫기 버튼 클릭 시, con 먼저 fade out 처리 후 비동기적으로 top, right, left, bottom 한번에 선이 사라지고 다시 open 버튼이 보이도록
