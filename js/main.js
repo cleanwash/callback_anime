33a8a9523987f9f154704c4097409164806ec62d
kenbugless <kenbugless@gmail.com>
2023-10-28 10:21:51 +0900

    css의 transition-duration값 밀리세컨드 변환함수 추가 및 적용

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const [topEl, rightEl, bottomEl, leftEl, conEl] = main.children;
 const btnClose = conEl.querySelector('span');
 const btn = document.querySelector('button');
 const speed = 500;
+const delayed = convertSpeed(conEl);
 
 btn.addEventListener('click', panelOpen);
 btnClose.addEventListener('click', panelClose);
@@ -56,7 +57,12 @@ function panelClose() {
 		[leftEl, rightEl].forEach((el) => new Anime(el, { height: '0%' }, { duration: speed }));
 		[bottomEl, topEl].forEach((el) => new Anime(el, { width: '0%' }, { duration: speed }));
 		btn.classList.remove('off');
-	}, 1000);
+	}, delayed);
+}
+
+function convertSpeed(el) {
+	//parseFloat, parseInt는 문자값이 같이 있는 문자열이더라도 숫자로 시작되는 문자열이면 자동으로 숫자로 형변환처리됨
+	return parseFloat(getComputedStyle(el).transitionDuration) * 1000;
 }
 
 //미션 - 닫기 버튼 클릭시 con 먼저 fade out 처리후 비동기적으로 top, right, left, bottom 한번에 선이 사라지고 다시 open버튼이 보이도록 처리
